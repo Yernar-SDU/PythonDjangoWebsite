98a3e97e38a1ca3f3a54963b1aeb39a5cfe664ed
Yernar-SDU <ernar200201kz@gmail.com>
2023-04-12 16:16:05 +0600

    added login page

--- iherb/iherbItems/urls.py
+++ iherb/iherbItems/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('login/', views.login_page, name='login_page')
 ]
--- iherb/iherbItems/views.py
+++ iherb/iherbItems/views.py
@@ -7,3 +7,7 @@ def home_page(request):
     items = Item.objects.all()
     context = {'items': items}
     return render(request, 'ogani-master/index.html', context)
+
+
+def login_page(request):
+    return render(request, 'ogani-master/login.html')
