e156d86e12b44b3996d9daac920e26b4eaaa6a2b
Yernar-SDU <ernar200201kz@gmail.com>
2023-04-12 17:03:30 +0600

    login function is not working

--- iherb/iherbItems/views.py
+++ iherb/iherbItems/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Item
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
@@ -11,3 +13,16 @@ def home_page(request):
 
 def login_page(request):
     return render(request, 'ogani-master/login.html')
+
+@login_required(login_url="")
+def login(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # A backend authenticated the credentials
+        ...
+    else:
+        # No backend authenticated the credentials
+        ...
